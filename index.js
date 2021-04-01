f23ff5008569be7159ef51799c7aa4d44629d29f
Musiur Alam Opu <musiuralamo@gmail.com>
2021-04-02 04:55:56 +0600

    after editing the readme file

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.connect(err => {
       const id = ObjectID(req.params.id);//
       console.log("delete:", id);
       logoCollection.findOneAndDelete({_id: id})
-      .then(documents => res.redirect('../'))
+      .then(documents => res.redirect('../ManageLogo'))
     })
 //posting order details
 app.post('/addOrder', (req, res) => {
